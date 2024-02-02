00766372b4473f7ece0985cc46dd51bf506b2648
Emir <emirkan.ulker9@gmail.com>
2024-02-02 13:26:12 +0300

    test

---
+++ Test-resim/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Test_resim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRenkSec = new System.Windows.Forms.Button();
+            this.BtnResimSec = new System.Windows.Forms.Button();
+            this.btnYazdır = new System.Windows.Forms.Button();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.txtBoyut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRenkSec
+            // 
+            this.btnRenkSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRenkSec.Location = new System.Drawing.Point(55, 77);
+            this.btnRenkSec.Name = "btnRenkSec";
+            this.btnRenkSec.Size = new System.Drawing.Size(118, 32);
+            this.btnRenkSec.TabIndex = 2;
+            this.btnRenkSec.Text = "Renk Seç";
+            this.btnRenkSec.UseVisualStyleBackColor = true;
+            this.btnRenkSec.Click += new System.EventHandler(this.btnRenkSec_Click);
+            // 
+            // BtnResimSec
+            // 
+            this.BtnResimSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnResimSec.Location = new System.Drawing.Point(55, 26);
+            this.BtnResimSec.Name = "BtnResimSec";
+            this.BtnResimSec.Size = new System.Drawing.Size(118, 32);
+            this.BtnResimSec.TabIndex = 1;
+            this.BtnResimSec.Text = "Resim Seç";
+            this.BtnResimSec.UseVisualStyleBackColor = true;
+            this.BtnResimSec.Click += new System.EventHandler(this.BtnResimSec_Click);
+            // 
+            // btnYazdır
+            // 
+            this.btnYazdır.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYazdır.Location = new System.Drawing.Point(55, 125);
+            this.btnYazdır.Name = "btnYazdır";
+            this.btnYazdır.Size = new System.Drawing.Size(118, 32);
+            this.btnYazdır.TabIndex = 3;
+            this.btnYazdır.Text = "Yazdır";
+            this.btnYazdır.UseVisualStyleBackColor = true;
+            this.btnYazdır.Click += new System.EventHandler(this.btnYazdır_Click);
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKaydet.Location = new System.Drawing.Point(55, 173);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(118, 32);
+            this.BtnKaydet.TabIndex = 4;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Metin:";
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMetin.Location = new System.Drawing.Point(73, 239);
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(100, 24);
+            this.txtMetin.TabIndex = 5;
+            // 
+            // txtBoyut
+            // 
+            this.txtBoyut.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBoyut.Location = new System.Drawing.Point(73, 278);
+            this.txtBoyut.Name = "txtBoyut";
+            this.txtBoyut.Size = new System.Drawing.Size(100, 24);
+            this.txtBoyut.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Boyut:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(217, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(441, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 332);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtBoyut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMetin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.btnYazdır);
+            this.Controls.Add(this.BtnResimSec);
+            this.Controls.Add(this.btnRenkSec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRenkSec;
+        private System.Windows.Forms.Button BtnResimSec;
+        private System.Windows.Forms.Button btnYazdır;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.TextBox txtBoyut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Test-resim/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_resim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string resim;
+        Color renk;
+
+        private void BtnResimSec_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            resim = openFileDialog1.FileName;
+        }
+
+        private void btnRenkSec_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            renk = colorDialog1.Color;
+        }
+
+        Bitmap bmp;
+        private void btnYazdır_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(resim);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.DrawString(txtMetin.Text, new Font("Arial", Convert.ToInt16(txtBoyut.Text), FontStyle.Bold), new SolidBrush(renk),30,50);
+            pictureBox1.Image = bmp;
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Resim |.jpg";
+            saveFileDialog1.ShowDialog();
+            bmp.Save(saveFileDialog1.FileName);
+        }
+    }
+}
